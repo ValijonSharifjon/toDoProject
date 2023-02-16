0103f22d85b8ec79c5c299d6e27f64607110c532
Valijon <sharifjonovvalijon@gmail.com>
2023-02-16 21:44:38 +0500

    update

--- todos.js
+++ todos.js
@@ -6,8 +6,12 @@ const request1=async()=>{
     const response1=await fetch(`https://jsonplaceholder.typicode.com/todos/${result}`)
     const data1=await response1.json()
     const p1=document.createElement("p")
-    p1.innerHTML=data1.title
-    LIST1.append(p1)
-    console.log(window.location)
+    p1.innerHTML=`id:${data1.id}`
+    const p2=document.createElement("p")
+    p2.innerHTML=`title:${data1.title}`
+    const p3=document.createElement("p")
+    p3.innerHTML=`userId:${data1.userId}`
+    LIST1.append(p1,p2,p3)
+    console.log(data1)
 }
 request1()
